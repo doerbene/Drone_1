ea2eda20efbad72e4e621b01ca06aa7cb3c9b22f
Cedced-Bro <cedric.boes@online.de>
2017-09-17 23:13:48 +0200

    Mayor Patch with sensor support
    In this patch I tried to implement the accelaration-sensor, the gyroscope and the temperature-sensor which may not be needed but is included in the sensor which was ordered.
    In order to add multi-sensor support I will maybe add a config for the registers soon. At the moment you still have to change everything just in the code.
    
    Other patchnotes:
    *Fixed some obvious bugs
    
    IMPORTANT: Code is not tested as the sensor hasn't already arrived.

--- controller/smartdrone1.py
+++ controller/smartdrone1.py
@@ -2,13 +2,18 @@ import sys
 import io
 import logging
 import time
-import RPi.GPIO as GPIO
 from datetime import datetime
 from configparser import ConfigParser
+try:                                        # You have to install those two modules
+    import smbus
+    import RPi.GPIO as GPIO
+except:
+    print("[Error] Error while importing smbus and RPi")
+    sys.exit(1)
 
 
 class Vector:
-    __init__(self, xstart=0, ystart=0, zstart=0 , x, y, z):
+    def __init__(self, x, y, z, xstart=0, ystart=0, zstart=0):
         self.x = x
         self.y = y
         self.z = z
@@ -16,7 +21,7 @@ class Vector:
         self.ystart = ystart
         self.zstart = zstart
 
-    __init__(self, x, y, z, roll, pitch, yaw):
+    def __init__(self, x, y, z, roll, pitch, yaw):
         self.x = x
         self.y = y
         self.z = z
@@ -24,12 +29,66 @@ class Vector:
         self.pitch = pitch
         self.yaw = yaw
 
+    def __init__(self, a, b, c, angles=False):
+        if angles:
+            self.roll = a%360
+            self.pitch = b%360
+            self.yaw = c%360
+        else:
+            self.x = a
+            self.y = b
+            self.z = c
+
 
 class Sensors:
-    def get_accelaration(sensor=0): # Sensor here is just for later additions to the drone and won't affect this program in any wise at this point
-        pass
-    def get_gyro(sensor=0):         # Sensor here is just for later additions to the drone and won't affect this program in any wise at this point
-        pass
+    # Registers: TODO: Put them into a config to allow other users to switch sensors more easy
+    # Accelaration
+    REG_ACCELX = [0x3b, 0x3c]       # These 2 Registers are the High and the low byte for our accelaration sensor in x direction
+    REG_ACCELY = [0x3d, 0x3e]       # These 2 Registers are the High and the low byte for our accelaration sensor in y direction
+    REG_ACCELZ = [0x3f, 0x40]       # These 2 Registers are the High and the low byte for our accelaration sensor in z direction
+
+    # Temperature
+    REG_TEMP = [0x41, 0x42]         # These 2 Registers are the High and the low byte for our temperature sensor
+
+    # Gyroscope
+    REG_GYROX = [0x43, 0x44]        # These 2 Registers are the High and the low byte for our gyroscope sensor in x direction (roll)
+    REG_GYROY = [0x45, 0x46]        # These 2 Registers are the High and the low byte for our gyroscope sensor in y direction (pitch)
+    REG_GYROZ = [0x47, 0x48]        # These 2 Registers are the High and the low byte for our gyroscope sensor in z direction (yaw)
+
+    def get_accelaration():
+        accelx_high = bus.read_byte(REG_ACCELX[0])
+        accelx_low = bus.read_byte(REG_ACCELX[1])
+        accely_high = bus.read_byte(REG_ACCELY[0])
+        accely_low = bus.read_byte(REG_ACCELY[1])
+        accelz_high = bus.read_byte(REG_ACCELZ[0])
+        accelz_low = bus.read_byte(REG_ACCELZ[1])
+
+        accelx = (accelx_high << 8) + accelx_low
+        accely = (accely_high << 8) + accely_low
+        accelz = (accelz_high << 8) + accelz_low
+
+        return Vector(accelx, accely, accelz)
+
+    def get_gyro():
+        gyrox_high = bus.read_byte(REG_GYROX[0])
+        gyrox_low = bus.read_byte(REG_GYROX[1])
+        gyroy_high = bus.read_byte(REG_GYROY[0])
+        gyroy_low = bus.read_byte(REG_GYROY[1])
+        gyroz_high = bus.read_byte(REG_GYROZ[0])
+        gyroz_low = bus.read_byte(REG_GYROZ[1])
+
+        gyrox = (gyrox_high << 8) + gyrox_low
+        gyroy = (gyroy_high << 8) + gyroy_low
+        gyroz = (gyroz_high << 8) + gyroz_low
+
+        return Vector(gyrox, gyroy, gyroz, angles=True)     # TODO: Check if the sensor really gives back an angle
+
+    def get_temp():
+        temp_high = bus.read_byte(REG_TEMP[0])
+        temp_low = bus.read_byte(REG_TEMP[1])
+        temp = (temp_high << 8) + temp_low
+        return temp
+
     def get_ultrasonic(sensor):     # Sensor is the Sensor which you want to get the ultrasonic information from
         pass
     def get_gps_location():
@@ -39,7 +98,7 @@ class Sensors:
 
 
 class Engine:
-    def set_voltage_level(engine):
+    def set_voltage_level(engine, voltage):
         pass
     def get_voltage_level(engine):
         pass
@@ -65,6 +124,12 @@ class Control_mode:
         pass
     def accelerate(direction):
         pass
+    def return_home():
+        pass
+
+
+class NetworkManager:
+    pass
 
 
 class Logger:           # if an instance is made the program can switch between a debug mode where everything will be logged and a normal mode with no outputs
@@ -74,8 +139,8 @@ class Logger:           # if an instance is made the program can switch between 
                 logging.basicConfig(filename="logs/drone1_(ver."+version+")_" + self.get_time("date") + ".log", format=self.get_time() + ' - %(name)s - %(levelname)s - %(message)s')
                 root = logging.getLogger()
                 io = logging.StreamHandler(sys.stdout)
-                io.setLevel(logging.DEBUG)
                 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+                io.setLevel(logging.DEBUG)
                 io.setFormatter(formatter)
                 root.addHandler(io)
                 root.setLevel(logging.DEBUG)
@@ -92,8 +157,9 @@ class Logger:           # if an instance is made the program can switch between 
         else:
             return (str(date.year) + "-" + str(date.month) + "-" + str(date.day) + " " + str(date.hour) + ":" + str(date.minute) + ":" + str(date.second) + "'" + str(date.microsecond))
 
+
 class ConfigLoader:
-    def get_string(config_file="smartdrone1.conf", section, attribute):
+    def get_string(section, attribute, config_file="smartdrone1.conf"):
         config = ConfigParser.read(config_file)
         config.sections()
         if not config.has_section(section):
@@ -104,7 +170,7 @@ class ConfigLoader:
             print("[Warning] The result is None")
         return result
 
-    def set_string(config_file="smartdrone1.conf", section, attribute, value):
+    def set_string(section, attribute, value, config_file="smartdrone1.conf"):
         config = ConfigParser.read(config_file)
         if config.has_section(section) is False:
             config.add_section(section)
@@ -122,3 +188,5 @@ if log.logging:
     print("[Info] Debug-mode started. Fix your errors :)")
 
 print("[Info] Logger successfully initialized")
+
+bus = smbus.SMBus(1)
